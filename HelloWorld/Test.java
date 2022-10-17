1509bec6a185ebaa7d6a81c3c3a61e61974cb4eb
rksundaram76 <81460613+rksundaram76@users.noreply.github.com>
2022-10-17 11:45:09 +0000

    Update Test.java

--- HelloWorld/Test.java
+++ HelloWorld/Test.java
@@ -6,6 +6,6 @@ public class Test
     {
         System.out.println("Test");
         System.out.println("Test1");
-        System.out.println("syntax error);
+        System.out.println("syntax error");
     }
 }
